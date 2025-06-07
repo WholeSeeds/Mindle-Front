620da9b2ebc0fb6da18c3afce497ff9e0710991b
jm3789 <jm37890@gmail.com>
2025-06-07 13:38:20 +0900

    chore: 확인용페이지 경로변경

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: const Text('홈')));
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            const Text('홈'),
+            TextButton(
+              onPressed: () => Get.toNamed('/inside'),
+              child: const Text('다음 페이지'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/pages/map_page.dart
+++ lib/pages/map_page.dart
@@ -1,24 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 
 class MapPage extends StatelessWidget {
   const MapPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            const Text('민원작성'),
-            TextButton(
-              onPressed: () => Get.toNamed('/inside'),
-              child: const Text('다음 페이지'),
-            ),
-          ],
-        ),
-      ),
-    );
+    return Scaffold(body: Center(child: const Text('민원작성')));
   }
 }
