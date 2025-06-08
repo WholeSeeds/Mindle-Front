dfda7463cd8203dca60cd24d894609f7979b2550
jm3789 <jm37890@gmail.com>
2025-06-08 19:26:42 +0900

    feat: 네이버맵 초기화 후 표시

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_naver_map/flutter_naver_map.dart';
 import 'package:mindle/controllers/bottom_nav_controller.dart';
 import 'package:mindle/bottom_nav_items.dart';
 import 'package:mindle/route_pages.dart';
 import 'package:mindle/widgets/mindle_bottom_navigation_bar.dart';
 import 'package:get/get.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await dotenv.load(fileName: ".env");
+
+  // 네이버 지도 초기화: client Id를 지정
+  await FlutterNaverMap().init(
+    clientId: dotenv.env['NAVER_MAP_CLIENT_ID'] ?? '',
+    onAuthFailed: (ex) => switch (ex) {
+      NQuotaExceededException(:final message) => print(
+        "사용량 초과 (message: $message)",
+      ),
+      NUnauthorizedClientException() ||
+      NClientUnspecifiedException() ||
+      NAnotherAuthFailedException() => print("인증 실패: $ex"),
+    },
+  );
+
   Get.put(BottomNavController());
+
   runApp(const MyApp());
 }
 
--- lib/pages/map_page.dart
+++ lib/pages/map_page.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_naver_map/flutter_naver_map.dart';
 
 class MapPage extends StatelessWidget {
   const MapPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: const Text('민원작성')));
+    return Scaffold(
+      body: NaverMap(
+        onMapReady: (controller) {
+          print("네이버맵 로딩완료");
+        },
+      ),
+    );
   }
 }
