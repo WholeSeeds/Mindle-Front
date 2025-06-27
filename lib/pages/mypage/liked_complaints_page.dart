b30470d8ef9bc31458836c9883c969b24befbff9
godeka <godeka@naver.com>
2025-06-27 17:04:32 +0900

    fix: report -> complaint 수정 반영 #23

--- lib/pages/mypage/liked_complaints_page.dart
+++ lib/pages/mypage/liked_complaints_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:mindle/widgets/align_options_button.dart';
-import 'package:mindle/widgets/report_card.dart';
+import 'package:mindle/widgets/complaint_card.dart';
 
 class LikedComplaintsPage extends StatelessWidget {
   const LikedComplaintsPage({super.key});
@@ -13,21 +13,21 @@ class LikedComplaintsPage extends StatelessWidget {
       body: Column(
         children: [
           AlignOptionsButton(),
-          ReportCard(
+          ComplaintCard(
             title: "불만사항 1",
             content: "그냥 짜증나요;;",
             numLikes: 100,
             numComments: 22,
             status: "no",
           ),
-          ReportCard(
+          ComplaintCard(
             title: "불만사항 2",
             content: "열기도 더 더해지고 너무 힘들어요.",
             numLikes: 18,
             numComments: 2,
             status: "accepted",
           ),
-          ReportCard(
+          ComplaintCard(
             title: "불만사항 3",
             content: "아아아",
             numLikes: 5,
